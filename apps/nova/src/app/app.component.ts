5b0db3fc90f5e60bdbbe2c894ee47b17968b888c
rbanning <rob@myhallpass.com>
2024-02-27 07:18:51 -0800

    create ui lib and add component to app

--- apps/nova/src/app/app.component.ts
+++ apps/nova/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './_components/welcome/welcome.component';
 import { FooterComponent } from './_components/footer/footer.component';
+import { ButtonComponent } from '@ng-sandbox-nova/core-ui';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, WelcomeComponent, FooterComponent],
+  imports: [RouterModule, WelcomeComponent, FooterComponent, ButtonComponent],
   selector: 'nova-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
