ae1a9cb1ecda1f52637b9fdee4e72aaa41b974f9
rbanning <rob@myhallpass.com>
2024-02-27 08:53:49 -0800

    add selectors to stories

--- libs/ui/core-ui/src/lib/button/button.component.stories.ts
+++ libs/ui/core-ui/src/lib/button/button.component.stories.ts
@@ -1,9 +1,16 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { ButtonComponent } from './button.component';
+import { storySelectCtrl } from '@ng-sandbox-nova/storybook-helpers';
+import { themeColorBGWithTransparentList, themeColorBasicList, themeColorBlackWhite, themeColorIntensityList, themeColorOpacityList } from '@ng-sandbox-nova/styles';
 
 const meta: Meta<ButtonComponent> = {
   component: ButtonComponent,
   title: 'ButtonComponent',
+  argTypes: {
+    ...storySelectCtrl('bgColor', [...themeColorBGWithTransparentList, ...themeColorBasicList, ...themeColorBlackWhite]),
+    ...storySelectCtrl('bgIntensity', themeColorIntensityList),
+    ...storySelectCtrl('bgOpacity', themeColorOpacityList),
+  }
 };
 export default meta;
 type Story = StoryObj<ButtonComponent>;
